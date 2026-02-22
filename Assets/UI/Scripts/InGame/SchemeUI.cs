b12b0630cd20ab55571fdc15e084725e0b27178e
chan527 <chan020527@gmail.com>
2026-02-22 21:55:03 +0900

    공작 선택 UI 구현
    SchemeUI 하위에 구현
    우측 하단 UI를 켜기 위한 임시 ShowPlot 버튼 생성

--- Assets/UI/Scripts/InGame/SchemeUI.cs
+++ Assets/UI/Scripts/InGame/SchemeUI.cs
@@ -1,6 +1,78 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 
 public class SchemeUI : MonoBehaviour
 {
-    
-}
+    [Header("--- 공작 선택 UI ---")]
+    public GameObject plotSelectUI;
+    public Image[] plotPanels = new Image[3];
+    public Button[] plotButtons = new Button[3];
+
+    public void ShowPlotUI(int npcID)
+    {
+
+        // SetPlotUI(npcID); UI 정보 업데이트 함수
+
+        plotSelectUI.SetActive(true);
+
+        /* 다른 상호작용 버튼 비활성화
+        foreach (var item in actionButtons)
+        {
+            item.interactable = false;
+        }*/
+    }
+
+    public void OnClickClose()
+    {
+        plotSelectUI.SetActive(false);
+
+        /* 다른 상호작용 버튼 활성화
+        foreach (var item in actionButtons)
+        {
+            item.interactable = true;
+        }*/
+    }
+
+    public void SetPlotUI(int npcID)
+    {
+        ResetPlotUI();
+
+        /*
+        int count = gameManager.GetAvailPlotCount(npcID);
+
+        for (int i = 0; i < count; i++)
+        {
+            plotNames[i].text = gameManager.GetPlotName(npcID, i);     // 공작 이름 전달
+            plotDescs[i].text = gameManager.GetPlotDesc(npcID, i);     // 공작 설명 전달
+            plotEffect[i].text = gameManager.GetPlotEffectText(npcID, i);     // 공작 효과 및 비용 전달
+            if (gameManager.CheckPlotUsed(npcID, i))
+            {
+                Debug.Log("사용됨");
+                plotPanels[i].color = new Color(0.8f, 0.8f, 0.8f);
+                plotButtons[i].interactable = false;
+            }
+            else
+            {
+                Debug.Log("사용안됨");
+                plotPanels[i].color = new Color(1f, 1f, 1f);
+                plotButtons[i].interactable = gameManager.CheckCanSelect(npcID, i);
+            }
+        }*/
+    }
+
+    // 공작 UI 정보 리셋 함수
+    public void ResetPlotUI()
+    {
+        /* 공작 UI 정보 리셋
+        for (int i = 0; i < 3; i++)
+        {
+            plotNames[i].text = "";
+            plotDescs[i].text = "";
+            plotEffect[i].text = "";
+            plotButtons[i].interactable = false;
+        }*/
+    }
+    }
