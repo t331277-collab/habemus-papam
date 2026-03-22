d9b25189ed5cec647092b80f917f1e2f4c40e476
chan527 <chan020527@gmail.com>
2026-03-22 19:53:38 +0900

    feat: P021 구현
    -P021 구현

--- Assets/Plot/Scripts/Plot List/P021.cs
+++ Assets/Plot/Scripts/Plot List/P021.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
+using static GameContext;
+using System.Collections;
+using System.Collections.Generic;
 
-[CreateAssetMenu(fileName = "P021", menuName = "Plot/무릎 꿇기(미구현)", order = 021)]
+[CreateAssetMenu(fileName = "P021", menuName = "Plot/무릎 꿇기", order = 021)]
 
 public class P021 : Plot
 {
@@ -8,6 +11,8 @@ public class P021 : Plot
     [SerializeField] private int minInfluence;
     [SerializeField] private int pietyCost;
     [SerializeField] private int influenceDelta;
+    [SerializeField] private int baseNextDayInfluenceDelta;
+    [SerializeField] private int influenceGainPerConclave;
 
     public override int cost => pietyCost;
 
@@ -24,9 +29,11 @@ public class P021 : Plot
         minInfluence = 55;
         pietyCost = 10;
         influenceDelta = -15;
+        baseNextDayInfluenceDelta = 20;
+        influenceGainPerConclave = 10;
 
         // 텍스트 기본값
-        plotName = "무릎 꿇기(미구현)";
+        plotName = "무릎 꿇기";
         plotDescription = "추진력을 얻기 위함이었다!";
         plotEffect = "모든 상대 후보 정치력<sprite name=influence> 15 감소\n다음 날 첫 콘클라베 때 지난 콘클라베 수에 따라 모든 상대 후보 정치력<sprite name=influence> 20 ~ 50 증가 ";
         plotCondiText = $"<sprite name=influence>{minInfluence}<sprite name=up>";
@@ -45,6 +52,8 @@ public class P021 : Plot
 
         performer.ChangePiety(-pietyCost);
 
+        Debug.Log("P021 사용");
+
         var cm = CardinalManager.Instance;
 
         for (int i = 0; i < 3; i++)
@@ -54,7 +63,50 @@ public class P021 : Plot
 
         /*
          * 다음 날 콘클라베가 되면 다시 정치력 증가하는 함수
+         * 다음날쓰
+         * 공작 시점 이브닝20, 애프터눈30, 모닝40, 던50
          */
+        PlotManager.Instance.StartCoroutine(CountConclaveProcess(performer));
     }
 
+    private IEnumerator CountConclaveProcess(Cardinal performer)
+    {
+        var gm = InGameManager.Instance;
+
+        int currentDay = gm.GetCurrentDay();
+        int conclaveCount = 0;
+
+        bool isNextDayStarted = false;
+
+        System.Action<GameContextEvent> eventHandler = (evt) => {
+            if (evt == GameContextEvent.ConclaveEnd)
+            {
+                conclaveCount++;
+            }
+
+            if (evt == GameContextEvent.ConclaveStart)
+            {
+                if (currentDay < gm.GetCurrentDay())
+                {
+                    isNextDayStarted = true;
+                }
+            }
+        };
+
+        InGameManager.Instance.Context.OnGameContextEvent += eventHandler;
+
+        yield return new WaitUntil(() => isNextDayStarted);
+
+        InGameManager.Instance.Context.OnGameContextEvent -= eventHandler;
+
+        var cm = CardinalManager.Instance;
+
+        int totalInfluenceDelta = ((conclaveCount - 1) * influenceGainPerConclave) + baseNextDayInfluenceDelta;
+
+        for (int i = 0; i < 3; i++)
+        {
+            cm.Cardinals[i].ChangeInfluence(totalInfluenceDelta);
+        }
+
+    }
 }
