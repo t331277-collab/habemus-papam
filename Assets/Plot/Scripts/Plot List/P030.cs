9b07227f37c6c6fc1f8d08bcabf1a93f963c312e
chan527 <chan020527@gmail.com>
2026-03-28 22:11:43 +0900

    feat: P030 구현, 최소 체력 1 구현
    - P030 구현
    - Cardinal.cs 최소 체력 1 구현

--- Assets/Plot/Scripts/Plot List/P030.cs
+++ Assets/Plot/Scripts/Plot List/P030.cs
@@ -1,7 +1,9 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
-[CreateAssetMenu(fileName = "P030", menuName = "Plot/결코 다시 전쟁!(미구현)", order = 030)]
+[CreateAssetMenu(fileName = "P030", menuName = "Plot/결코 다시 전쟁!", order = 030)]
 
 public class P030 : Plot
 {
@@ -27,7 +29,7 @@ public class P030 : Plot
         influenceDelta = 20;
 
         // 텍스트 기본값
-        plotName = "결코 다시 전쟁!(미구현)";
+        plotName = "결코 다시 전쟁!";
         plotDescription = "성전이다, 우매한 이단들아!";
         plotEffect = "모든 후보 정치력<sprite name=influence> 40 증가\n이번 콘클라베 동안 자신의 체력<sprite name=hp> 최소 1 유지";
         plotCondiText = $"<sprite name=influence>{minInfluence}<sprite name=up>";
@@ -60,9 +62,43 @@ public class P030 : Plot
             cm.Cardinals[i].ChangeInfluence(influenceDelta);
         }
 
-        /*
-         * 이번 콘클라베 동안 모든 후보 체력 최소 1 유지
-         */
+        performer.StartCoroutine(Co_ApplyMinHpOneUntilConclaveEnd(performer));
     }
 
+    private IEnumerator Co_ApplyMinHpOneUntilConclaveEnd(Cardinal performer)
+    {
+        if (performer == null || InGameManager.Instance == null || InGameManager.Instance.Context == null)
+        {
+            yield break;
+        }
+
+        GameContext context = InGameManager.Instance.Context;
+        bool isEnded = false;
+
+        void OnContextEvent(GameContext.GameContextEvent eventType)
+        {
+            if (eventType == GameContext.GameContextEvent.ConclaveEnd)
+            {
+                isEnded = true;
+            }
+        }
+
+        performer.SetMinHpOneEffect(true);
+        Debug.Log("최소 체력 1 ON");
+        context.OnGameContextEvent += OnContextEvent;
+
+        yield return new WaitUntil(() => isEnded || performer == null);
+
+        if (performer != null)
+        {
+            performer.SetMinHpOneEffect(false);
+            Debug.Log("최소 체력 1 OFF");
+        }
+
+        if (context != null)
+        {
+            context.OnGameContextEvent -= OnContextEvent;
+        }
+    }
 }
+
